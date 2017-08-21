f16428742c4a19d68d8d39f276a2cfea2170df4d
Betty Chow <bettychchow@gmail.com>
2017-08-20 20:34:46 -0700

    first commit

---
+++ src/components/posts_new.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Field, reduxForm } from 'redux-form'
+
+class PostsNew extends Component {
+
+  renderTitleField = (field) => {
+    return (
+      <div>
+        <input 
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+      <div className="text-xs-right">
+        <Link className="btn btn-primary" to="/">
+          Back
+        </Link>
+      </div>
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+        </form>
+      </div>
+   )
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew)
+
